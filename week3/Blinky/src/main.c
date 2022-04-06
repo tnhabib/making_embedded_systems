acd447122219575e4cd7d14f21c5d639cecb3ed5
tnhabib <tnhabib@gmail.com>
2022-04-05 20:26:16 -0700

    added code to having red/green chase effect

--- week3/Blinky/src/main.c
+++ week3/Blinky/src/main.c
@@ -1,18 +1,26 @@
 #include "main.h"
 
-#define LED_PIN GPIO_PIN_13
+#define GREEN_LED_PIN GPIO_PIN_13
+#define RED_LED_PIN GPIO_PIN_14
 #define LED_GPIO_PORT GPIOG
 #define LED_GPIO_CLK_ENABLE() __HAL_RCC_GPIOG_CLK_ENABLE()
 
 void LED_Init();
 
 int main(void) {
+    volatile int counter = 0;
     HAL_Init();
     LED_Init();
     while (1)
     {
-        HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+        if (counter % 2 == 0) {
+            HAL_GPIO_TogglePin(LED_GPIO_PORT, GREEN_LED_PIN);
+        } else {
+           HAL_GPIO_TogglePin(LED_GPIO_PORT, RED_LED_PIN); 
+        }
+        
         HAL_Delay(1000);
+        counter++;
     }
     
     return 0;
@@ -21,7 +29,7 @@ int main(void) {
 void LED_Init() {
     LED_GPIO_CLK_ENABLE();
     GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_InitStruct.Pin = LED_PIN;
+    GPIO_InitStruct.Pin = GREEN_LED_PIN | RED_LED_PIN;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_SPEED_HIGH;
     HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
