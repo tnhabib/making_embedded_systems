59976d0e02ca91c9975e55b1246bf51f6220f9ae
tnhabib <tnhabib@gmail.com>
2022-05-11 20:52:21 -0700

    prototype for LED Matrix

---
+++ protoypes/led_matrix/src/main.c
@@ -0,0 +1,108 @@
+#include "main.h"
+#include <stdio.h>
+
+#include "HUB75ELib.h"
+#include "sample_bitmap.h"
+static void SystemClock_Config(void);
+void Error_Handler(void);
+
+#include "dwt_delay.h"
+
+int main(void) {
+ 
+
+    
+    unsigned char myBitmap[PIXELS_COUNT_IN_BYTES];
+    for (int ii=0; ii < PIXELS_COUNT_IN_BYTES; ii++) {
+      myBitmap[ii] = 255;
+    }
+   
+
+    HAL_Init();
+    SystemClock_Config();
+    // DWT_Init();
+    HUB75E_Init();
+    HUB75E_setDisplayBuffer(myBitmap);
+    HUB75E_setDisplayBrightness(BrightnessLevel1);
+    HUB75E_setDisplayColor(Red);
+    HUB75E_displayBufferPixels(myBitmap);
+    while(1) {
+        HUB75E_displayBufferPixels(myBitmap);
+        HUB75E_setDisplayColor(Red);
+    }
+
+}
+
+
+
+void SysTick_Handler(void) {
+  HAL_IncTick();
+}
+
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __PWR_CLK_ENABLE();
+  
+  /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 360;
+
+  //   RCC_OscInitStruct.PLL.PLLM = 26;
+  // RCC_OscInitStruct.PLL.PLLN = 240;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  // RCC_OscInitStruct.PLL.PLLQ = 4;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler();
+  }
+  
+  if(HAL_PWREx_ActivateOverDrive() != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler();
+  }
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler();
+  }
+}
+
+
+
+// copied from Cube IDE Generated code
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+
