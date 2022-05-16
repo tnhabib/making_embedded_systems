0db3606889c7321508383549fc695cb8eec1c0cd
tnhabib <tnhabib@gmail.com>
2022-05-15 17:09:26 -0700

    cleanup

--- gyro_motion_game/src/dwt_delay.c
+++ gyro_motion_game/src/dwt_delay.c
@@ -31,13 +31,8 @@
  */
 int DWT_Init(void)
 {
-    // if (!(CoreDebug->DEMCR & CoreDebug_DEMCR_TRCENA_Msk)) {
-    //     CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
-    //     DWT->CYCCNT = 0;
-    //     DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
-    // }
 
-        /* Disable TRC */
+    /* Disable TRC */
     CoreDebug->DEMCR &= ~CoreDebug_DEMCR_TRCENA_Msk; // ~0x01000000;
     /* Enable TRC */
     CoreDebug->DEMCR |=  CoreDebug_DEMCR_TRCENA_Msk; // 0x01000000;
@@ -100,11 +95,9 @@ void DWT_Delay(uint32_t us) // microseconds
 void DWT_Delay(uint32_t us) // microseconds
 {
     uint32_t startTick = DWT->CYCCNT;
-    // uint32_t startTick = uwTick;
     uint32_t delayTicks = us * (SystemCoreClock/1000000);
 
     while (DWT->CYCCNT - startTick < delayTicks);
-    // while (uwTick - startTick < delayTicks);
-}
+
 
 #endif
